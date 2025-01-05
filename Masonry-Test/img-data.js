6db794ff85528ff0c99bded53fddef9a018d58d0
Nafis Tahmid <nafistahmid2020@gmail.com>
2025-01-05 19:44:34 +0600

    Adding Git

---
+++ Masonry-Test/img-data.js
@@ -0,0 +1,206 @@
+export let images = [
+  {
+    image: 'images/1RACS453BM2CX8T49GYA0CTSD0.jpeg'
+  },
+  {
+    image: 'images/2CBQ5YRWD92HSETZYJQDM28TS0.jpeg'
+  },
+  {
+    image: 'images/3B4NGS0FSAVQ2T87KT95RYV3N0.jpeg'
+  },
+  {
+    image: 'images/3b7409a1-2a92-48b2-b2a7-6604d6ba8700.jpeg'
+  },
+  {
+    image: 'images/3c8ae1aa-47c0-4fad-8afc-c5f301144f31.jpeg'
+  },
+  {
+    image: 'images/3f6f977d-9ff5-4045-a54c-08e4460a8700.jpeg'
+  },
+  {
+    image: 'images/3FZJHAE112YBX3YSXBRKGEJ8R0.jpeg'
+  },
+  {
+    image: 'images/3K9ZJAV0GC47FABJHZ12TWTTW0.jpeg'
+  },
+  {
+    image: 'images/4adf4d47-aa2b-4255-bb8b-112a8a44871a.jpeg'
+  },
+  {
+    image: 'images/4JYMGC4CFPKN4ANEH5XKSXSBV0.jpeg'
+  },
+  {
+    image: 'images/5Q7HBAKWNSRP1Z1WHB6AVFMBW0.jpeg'
+  },
+  {
+    image: 'images/5TFBXRDY4BZ8AAKW42ET8ZNFY0.jpeg'
+  },
+  {
+    image: 'images/05Z1H532CEAXQRN4YJJ6RGD2H0.jpeg'
+  },
+  {
+    image: 'images/8J0EKV1YA1AZNWEGRRTFC0HH90.jpeg'
+  },
+  {
+    image: 'images/8J0Q2FMEAQ1RWY2V1MQ1PGT3J0.jpeg'
+  },
+  {
+    image: 'images/9AEGSKB6ZR468GMZPR438FR1H0.jpeg'
+  },
+  {
+    image: 'images/9cf4c7b0-95ef-409c-8d16-86a7688bed00.jpeg'
+  },
+  {
+    image: 'images/9DYD11W2710VV0BS0J342HJ400.jpeg'
+  },
+  {
+    image: 'images/12c41ce2-e263-45ff-7781-a1acc98f1400.jpeg'
+  },
+  {
+    image: 'images/13FHVT7MZ19SGPCXE5GC18CB60.jpeg'
+  },
+  {
+    image: 'images/031c6c3d-fe21-4e38-b961-5c92e105ca7e.jpeg'
+  },
+  {
+    image: 'images/36b5c8a7-8ef6-447c-6d9d-a5a4606e1c00.jpeg'
+  },
+  {
+    image: 'images/37c83911-ce07-4ccf-9ed2-fd25bbdff6f8.jpeg'
+  },
+  {
+    image: 'images/39d8c882-ebfa-406d-f4cd-cc6e905a5d00.jpeg'
+  },
+  {
+    image: 'images/52d840e7-17fd-41a5-a30c-388a88efa13f.jpeg'
+  },
+  {
+    image: 'images/80N1465TSN7NZCEW4YPD0P1Z00.jpeg'
+  },
+  {
+    image: 'images/95cff2a2-ca3a-4741-b5b5-2f94e2fefe00.jpeg'
+  },
+  {
+    image: 'images/137ac9d7-4083-440a-66d8-5b8046109100.jpeg'
+  },
+  {
+    image: 'images/162c55d0-08a2-4f76-d637-6bbd81b79900.jpeg'
+  },
+  {
+    image: 'images/172QCD6PZGJ6V4GZBNTNDXR980.jpeg'
+  },
+  {
+    image: 'images/00230-2971847726.jpeg'
+  },
+  {
+    image: 'images/370E4QDSKZR27MXZYBGFTFBNK0 (1).jpeg'
+  },
+  {
+    image: 'images/639dbab5-416c-4bae-a176-787c03fe71e1.jpeg'
+  },
+  {
+    image: 'images/954TV1SCFHHX5YJPW0MV0NCR70.jpeg'
+  },
+  {
+    image: 'images/4122fba2-aebd-451c-602e-2fd08347ae00.jpeg'
+  },
+  {
+    image: 'images/514848eb-5df8-4281-854a-15bd08d88700.jpeg'
+  },
+  {
+    image: 'images/45676698.jpeg'
+  },
+  {
+    image: 'images/92870170-4228-46f1-99be-b48f25c3518d.jpeg'
+  },
+  {
+    image: 'images/a0eb5adf-43ec-4a2a-8c54-ce8a73c09ae0.jpeg'
+  },
+  {
+    image: 'images/ae10d06d-93cf-4d2c-9b11-57b456ab1ac1.jpeg'
+  },
+  {
+    image: 'images/bd89b63e-3a78-4077-28d9-1b6fbc133f00.jpeg'
+  },
+  {
+    image: 'images/bdcf50e4-0f23-47c3-b054-4e8af15a4e38.jpeg'
+  },
+  {
+    image: 'images/BDN4SQEKXNQ6HMJY8M9AFQNJD0.jpeg'
+  },
+  {
+    image: 'images/C82PCNF8NXN4NCG65JYQWY1GP0.jpeg'
+  },
+  {
+    image: 'images/c2185611-264d-4d80-b424-dd089bb7b059.jpeg'
+  },
+  {
+    image: 'images/cbeeb0d3-5092-44ff-464d-c0124c871300.jpeg'
+  },
+  {
+    image: 'images/dfea154c-24f6-47b2-9669-3a98700f2a7f.jpeg'
+  },
+  {
+    image: 'images/DV8W7V03X1VNSW26FAVX2S2WD0.jpeg'
+  },
+  {
+    image: 'images/DWRQWN6G7YEQCNTF5VJ8ZG4DX0.jpeg'
+  },
+  {
+    image: 'images/e75d9f2f-bd45-4080-aa66-f6c99c80a2e1.jpeg'
+  },
+  {
+    image: 'images/e92cea2e-cca5-4361-b880-721a441381cb.jpeg'
+  },
+  {
+    image: 'images/e169c50d-dde4-40c2-503f-6096ed355100.jpeg'
+  },
+  {
+    image: 'images/e175a9b4-36dd-4fc4-a559-9b0393f85162.jpeg'
+  },
+  {
+    image: 'images/e7385279-437b-47c6-b422-6f9ca8f57200.jpeg'
+  },
+  {
+    image: 'images/EH1H5A2P402EQ8ATYXJQ3FY4D0.jpeg'
+  },
+  {
+    image: 'images/EW0VBXAP8WM7T1GXYDR5G7T8H0.jpeg'
+  },
+  {
+    image: 'images/f8c06423-9d4a-4a52-bd10-a3c291da4284.jpeg'
+  },
+  {
+    image: 'images/GVDE986HKGJX90A0RDG9655X30.jpeg'
+  },
+  {
+    image: 'images/K7B0V36NYME8H53S3YKB3ENVR0.jpeg'
+  },
+  {
+    image: 'images/KP06B90QEF9R950671PEXZZ940.jpeg'
+  },
+  {
+    image: 'images/KTBE1AMV4MDN66HG4HKVKT8290.jpeg'
+  },
+  {
+    image: 'images/N0KDSA4ZYA4HMNFEAYQHVDP070.jpeg'
+  },
+  {
+    image: 'images/QD1FXW3Q5NBW26ZNM3M5W6SAZ0.jpeg'
+  },
+  {
+    image: 'images/RJDHZF8WF45PTGTG0GRPVK46Q0.jpeg'
+  },
+  {
+    image: 'images/SHBP6VD4PD3711DCQRDDBHE050.jpeg'
+  },
+  {
+    image: 'images/T4983W4YHJWHRRANHHNGN6QK50.jpeg'
+  },
+  {
+    image: 'images/Z2AQD4SPCMPG6HRVD8RJKY3Z00.jpeg'
+  },
+  {
+    image: 'images/WFTW03KQ8XKDQCN9EK5X6T'
+  }
+];
